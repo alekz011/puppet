f895b083b8590851266804c8fbbb5c3a600d895d
alekz011 <alekz@mail.com>
2015-03-16 10:35:19 +0100

    new nodes.pp file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,11 @@
 node 'ubuntu' ,'ubuntu2' {
-  include memcached
+#  include memcached
+  $packages = [ 'nmap',
+                'libblas3',
+                'liblinear1',
+                'liblua5.2-0',
+                'ndiff',
+                'python-lxml' ]
+
+package { $packages: ensure => installed }
 }
